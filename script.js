5b02d706dec2ccc91264b851d1e4143047968498
tamsynsteed <tamsynsteed@gmail.com>
2020-11-23 16:33:41 +0200

    completed html and css, starting java

---
+++ script.js
@@ -0,0 +1,9 @@
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+
+}
+function printHistory(num){
+    document.getElementById("history-value"),innerText=num;
+}
+printHistory("9*9+8");
+
